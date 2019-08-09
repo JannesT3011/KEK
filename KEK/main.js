3cb726dc805f198c539fffd844ef392e60331629
Jannes <36892200+Bmbus@users.noreply.github.com>
2019-08-09 14:56:22 +0200

    Update main.js
    Create emoji by url

--- KEK/main.js
+++ KEK/main.js
@@ -1,7 +1,24 @@
 const Discord = require("discord.js")
 const bot = new Discord.Client()
 const {TOKEN, GOOGLE_API_KEY, PREFIX} = require("./config")
-const bot.music = require("discord.js-musicbot-addon")
+bot.music = require("discord.js-musicbot-addon")
+
+bot.on("ready", function() {
+    console.log("Ready!")
+})
+
+bot.on("message", function(message) {
+    if (message.content.startsWith(PREFIX + "emoji")) {
+        emote_name = message.content.split(" ")[1]
+        img_url = message.content.split(" ")[2]
+        try {
+            message.guild.createEmoji(img_url, emote_name)
+            return message.channel.send("Emoji `" + emote_name + "` added!")
+        } catch (error) {
+            return message.channel.send("An error occured: " + error)
+        }
+    }
+})
 
 bot.music.start(bot, {
     youtubeKey: GOOGLE_API_KEY,
